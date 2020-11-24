2fe12e56b5fcb8257160bea5995df7d90eade8bd
quephuong.nguyen <extern.que.phuong.nguyen@volkswagen.de>
2020-11-24 09:37:11 +0100

    sales-person-table: created a table

---
+++ src/app/sales-person-list/sales-person-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import {SalesPerson} from "./sales-person";
+
+@Component({
+  selector: 'app-sales-person-list',
+  templateUrl: './sales-person-list.component.html',
+  styleUrls: ['./sales-person-list.component.css']
+})
+export class SalesPersonListComponent implements OnInit {
+
+  salesPersonList: SalesPerson[] = [
+    new SalesPerson("Anup", "Kumar", "anup.kumar@gmail.com", 50000),
+    new SalesPerson("John", "Doe", "john.doe@gmail.com", 40000),
+    new SalesPerson("Clair", "Murphy", "clair.murphy@gmail.com", 90000),
+    new SalesPerson("Mai", "Truong", "mai.truong@gmail.com", 60000)
+  ]
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
